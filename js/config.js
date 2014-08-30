c12dc61009859aa0a39f3b1436eb33ebe15697b5
Friedger <friedger@gmail.com>
2014-08-30 19:37:37 +0200

    rename to GDG Brussels, use European date format, remove sponsors

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 boomerang.factory('Config', function () {
     return {
         //modify these
-        'name'          : 'GTUG Brussels',
+        'name'          : 'GDG Brussels',
         'id'            : '105068877693379070381',
         'google_api'    : 'AIzaSyBuW84I4CwVyhC9F5lEmdgKjfHEABpopUM',
         'pwa_id'        : '5923085386435987489', //picasa web album id, must belong to google+ id above
